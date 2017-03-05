69e19d49b70ab3b040e794435d95a7fb243d25d3
Chantal Justamond <cjustamm66@me.com>
2017-03-04 18:31:58 -0700

    bug feedback not resolved

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,24 +1,28 @@
 class Guess
  
- attr_accessor :card, :guess
+ attr_accessor :card, :guess, :feedback, :correct
  
  def initialize(guess, card)
   @card = card
   @guess = guess
-  
  end
+ 
  
  def response
    @guess
  end
  def correct?
-  return false if @guess.empty?
-  @guess.include? @card.answer
-  
- end
- def feedback
-   correct? ? "Correct!" : "Incorrect. The answer is: #{@card.answer}" 
- end
+    #return false if @guess.empty?
+    @guess == @card.answer
+ end   
 
-
+  def feedback
+      # correct? ? "Correct!" : "Incorrect. The answer is: #{@card.answer}" 
+      #@guess == card.answer ? "Correct!" : "Incorrect. The answer is: #{@card.answer}"
+      if correct?
+      "Correct!"
+    else
+      "Incorrect."
+    end
+  end
 end
