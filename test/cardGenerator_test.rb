067594fe7f2c7f1f275f0f31a941e6374c8d0c5b
Chantal Justamond <cjustamm66@me.com>
2017-03-04 17:08:19 -0700

    pry eliminated

--- test/cardGenerator_test.rb
+++ test/cardGenerator_test.rb
@@ -6,7 +6,6 @@ require_relative '../lib/guess'
 require_relative '../lib/deck'
 require_relative '../lib/round'
 require_relative '../lib/card_generator'
-require 'pry'
 
 class CardGeneratorTest < Minitest::Test
 
